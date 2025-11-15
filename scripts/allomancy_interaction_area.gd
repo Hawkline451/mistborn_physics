966b72ab243f14d91e4cecdf49c4d30fe9beae48
unknown <joaquin.jjb@gmail.com>
2025-11-15 18:44:05 -0300

    fix(): working cone collision auto aim

--- scripts/allomancy_interaction_area.gd
+++ scripts/allomancy_interaction_area.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var marker: Node2D = $"../../../MetalMarker"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,13 +11,25 @@ func _ready() -> void:
 func _process(delta):
 	look_at(get_global_mouse_position())
 
-func _on_body_entered(body: Node2D) -> void:
-	if body is TileMapLayer:
-		print("Tile Detected", body)# Replace with function body.
-		
-
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+
 	if body is TileMapLayer:
-		var cell = body.get_coords_for_body_rid(body_rid)
+		# This works becaus TileMapLayer.Physics.Physics Quadrant = 1
+		var physics_quadrant_coords  = body.get_coords_for_body_rid(body_rid)
+
 		#body.highlight_cell(cell)
-		print("Tile Detected 2", cell) 
+		highlight_cell_marker(body, physics_quadrant_coords)
+		print("Tile Detected: ", physics_quadrant_coords) 
+		
+func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	#body.highlight_cell(cell)
+	hide_cell_marker(body, )
+		
+func highlight_cell_marker(tile:TileMapLayer, cell_position: Vector2i):
+	marker.show()
+	marker.position = tile.map_to_local(cell_position)
+	
+func hide_cell_marker(tile:TileMapLayer, ):
+	var out_of_view: Vector2i = Vector2i(-0,-0)
+	marker.hide()
+	marker.position = tile.map_to_local(out_of_view)
--- scripts/metal_tiles.gd
+++ scripts/metal_tiles.gd
@@ -9,8 +9,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var mouse_pos = get_local_mouse_position()
 	var cell_pos = local_to_map(mouse_pos)
-	print(cell_pos, "cell pos")
-	highlight_cell(cell_pos)
+	# Check if a tile exists at the calculated cell coordinates
+	var tile_data = get_cell_tile_data(cell_pos)
 
-func highlight_cell(cell_position: Vector2i):
+	if tile_data:
+		print(cell_pos, "mouse collision")
+		highlight_cell_marker(cell_pos)
+		# You can access tile_data properties here, e.g., tile_data.get_custom_data("Name")
+	else:
+		#hide_cell_marker()
+		pass
+
+func highlight_cell_marker(cell_position: Vector2i):
+	marker.show()
 	marker.position = map_to_local(cell_position)
+	
+func hide_cell_marker():
+	var out_of_view: Vector2i = Vector2i(-0,-0)
+	marker.hide()
+	marker.position = map_to_local(out_of_view)
